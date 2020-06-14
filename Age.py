d456a453b13e9216f0433f8d40b6f8f77d412949
Bambi Sanders <bambi@macbook-air-2.lan>
2020-06-14 18:54:52 -0400

    adding some fills and assignment 1

---
+++ Age.py
@@ -0,0 +1,4 @@
+print('how old are you')
+age = int(input())
+days = age * 365
+print('this is how many days you are old ' + str(days)) 
---
+++ Truthordare.py
@@ -0,0 +1,8 @@
+print('truth or dare ')
+x = input().lower()
+if 'truth' == x: 
+   print("What is something you love about yourself?")
+elif 'dare' == x:
+    print('Give someone you love a hug.')
+else:
+    print('Silly goose type truth or dare!')
