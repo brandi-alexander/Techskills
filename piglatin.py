ec9731cfe58a8bd6d12734b0104086880a7a410b
Bambi Sanders <bambi@macbook-air-2.lan>
2020-06-16 23:37:27 -0400

    I wrote piglatin!

---
+++ piglatin.py
@@ -0,0 +1,12 @@
+x = input('Enter a word here: ')
+con_suffix = 'ay'
+vowel_suffix = 'yay'
+first_letter = x[0]
+VOWELS = 'aeiou'
+
+if first_letter in VOWELS:
+    print(x + vowel_suffix)
+else:
+    print(x[1:] + first_letter + con_suffix)
+
+
