327b11f646ebb4820552adf74133ab63f7603bf9
Bambi Sanders <bambi@macbook-air-2.lan>
2020-06-20 20:06:04 -0400

    piglatinafied sentences

--- piglatin.py
+++ piglatin.py
@@ -1,10 +1,23 @@
-x = input('Enter a word here: ')
-con_suffix = 'ay'
-vowel_suffix = 'yay'
-first_letter = x[0]
-VOWELS = 'aeiou'
+def piglatinafy(word):
+    if '' == word:
+        return ''
+    else:
+        con_suffix = 'ay'
+        vowel_suffix = 'yay'
+        first_letter = word[0]
+        VOWELS = 'aeiou'
 
-if first_letter in VOWELS:
-    print(x + vowel_suffix)
-else:
-    print(x[1:] + first_letter + con_suffix)
+        if first_letter in VOWELS:
+            return word + vowel_suffix
+        else:
+            return word[1:] + first_letter + con_suffix
+
+x = input('Enter a sentence here: ')
+print(x.split(' '))
+answer = '' 
+for word in x.split(' '):
+    print(piglatinafy(word))
+    answer += ' ' + piglatinafy(word)
+print(answer.strip(' ') + '!')
+
+
