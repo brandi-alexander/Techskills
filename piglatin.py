44f82996c2e40544edadcea78fd54af0e369e914
Bambi Sanders <bambi@macbook-air-2.lan>
2020-07-04 22:26:17 -0400

    adding punctuation

--- piglatin.py
+++ piglatin.py
@@ -1,16 +1,23 @@
 def piglatinafy(word):
     if '' == word:
         return ''
+   
     else:
         con_suffix = 'ay'
         vowel_suffix = 'yay'
         first_letter = word[0]
         VOWELS = 'aeiou'
+        if  word[-1] in ',.;!?':
+            punctuation = word[-1]
+            word = word[:-1]
+        else:
+            punctuation = ''
 
         if first_letter in VOWELS:
-            return word + vowel_suffix
+            return word + vowel_suffix + punctuation
+
         else:
-            return word[1:] + first_letter + con_suffix
+            return word[1:] + first_letter + con_suffix + punctuation 
 
 x = input('Enter a sentence here: ')
 print(x.split(' '))
